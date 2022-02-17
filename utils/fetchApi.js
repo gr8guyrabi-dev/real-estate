7cf36dfa9f96247fd0d7563fbbea29f816728120
Rabi Maharjan <rabi.maharjan015@gmail.com>
2022-02-17 10:45:25 +0545

    set env variable name to support next.js app

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
         headers: {
             'x-rapidapi-host': 'bayut.p.rapidapi.com',
-            'x-rapidapi-key': process.env.RAPIDAPI_KEY
+            'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPIDAPI_KEY
           }
     })
 
